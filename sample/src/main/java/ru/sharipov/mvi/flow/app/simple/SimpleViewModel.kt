9db68bfefed5d1fea9059c0d90b2879d0b3e34a7
Margarita <margo.himera@yandex.ru>
2022-01-20 21:02:34 +0300

    Update MviViewModel

--- sample/src/main/java/ru/sharipov/mvi/flow/app/simple/SimpleViewModel.kt
+++ sample/src/main/java/ru/sharipov/mvi/flow/app/simple/SimpleViewModel.kt
@@ -1,21 +1,17 @@
 package ru.sharipov.mvi.flow.app.simple
 
-import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewModelScope
-import ru.sharipov.mvi.flow.*
-import ru.sharipov.mvi.flow.app.simple.SimpleEvent
-import ru.sharipov.mvi.flow.app.simple.SimpleMiddleware
-import ru.sharipov.mvi.flow.app.simple.SimpleReducer
-import ru.sharipov.mvi.flow.app.simple.SimpleState
+import ru.sharipov.mvi.flow.FlowEventHub
+import ru.sharipov.mvi.flow.FlowState
+import ru.sharipov.mvi.flow.lifecycle.MviViewModel
 
-class SimpleViewModel : ViewModel(), MviViewModel<SimpleState, SimpleEvent>, FlowBinder {
+class SimpleViewModel : MviViewModel<SimpleState, SimpleEvent>() {
 
     override val state: FlowState<SimpleState> = FlowState(SimpleState())
     override val hub: FlowEventHub<SimpleEvent> = FlowEventHub()
-    private val middleware: SimpleMiddleware = SimpleMiddleware(state)
-    private val reducer: SimpleReducer = SimpleReducer()
+    override val middleware: SimpleMiddleware = SimpleMiddleware(state)
+    override val reducer: SimpleReducer = SimpleReducer()
 
     init {
-        viewModelScope.bind(hub, middleware, state, reducer)
+        init()
     }
 }
